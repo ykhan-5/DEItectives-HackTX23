8f6c91abb3c54a1fb75c10da7d3bf3eb42f6acd5
ykhan-5 <131565514+ykhan-5@users.noreply.github.com>
2023-10-22 10:01:32 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,61 @@
+import math
+import json
+from flask import Flask, request, jsonify
+import json
+
+app = Flask(__name__)
+
+with open('colleges.json', 'r') as file:
+  json_data = file.read()
+coorddata = json.loads(json_data)
+
+
+def haversine(lat1, lon1, lat2, lon2):
+  R = 6371  # Radius of the Earth in kilometers
+
+  dlat = math.radians(lat2 - lat1)
+  dlon = math.radians(lon2 - lon1)
+
+  a = math.sin(dlat / 2) * math.sin(dlat / 2) + math.cos(math.radians(lat1))
+  b = math.cos(math.radians(lat2)) * math.sin(dlon / 2) * math.sin(dlon / 2)
+
+  c = a * b
+
+  d = 2 * math.atan2(math.sqrt(c), math.sqrt(1 - c))
+
+  distance = R * d
+  return distance
+
+
+def get_sorted_universities(lon, lat):
+  universities = coorddata["universities"]
+
+  # closest = get_closest_university(lon, lat)
+  # universities.remove(closest)
+  def comp(university):
+    university["distance"] = haversine(lon, lat, university["longitude"],
+                                       university["latitude"])
+
+    return haversine(lon, lat, float(university["coordinates"]["longitude"]),
+                     float(university["coordinates"]["latitude"]))
+
+  universities.sort(key=comp)
+  # print(universities)
+  return universities
+
+
+@app.route('/get_sorted_universities/<float:lon>,<float:lat>', methods=['GET'])
+def doGet(e):
+  lon = e.parameter["latitude"]
+  lat = e.parameter["lan"]
+  result = []
+  for university in get_sorted_universities(lon, lat):
+    result.append({
+        "name": university["name"],
+        "distance": university["distance"],
+        "address": university["address"],
+        "website_url": university["website_url"],
+        "tuition": university["tuition"]
+    })
+
+  return result
